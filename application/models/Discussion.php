3fee960dd469c6115a1c90ad06493c26db5b0863
foritstep <foritstep@gmail.com>
2017-12-22 17:39:14 +0200

    код для преобразования bb тегов вынесен в отдельный файл

--- application/models/Discussion.php
+++ application/models/Discussion.php
@@ -18,7 +18,7 @@ use yii\web\Request;
  */
 class Discussion extends \yii\db\ActiveRecord
 {
-    private $_dom;
+    use BB2HtmlTrait;
 
     public function behaviors()
     {
@@ -30,10 +30,7 @@ class Discussion extends \yii\db\ActiveRecord
                 'codeDefinitionBuilder' => [
                     // HACK id указан пустым, как следствие он приклеится из значения {option}
                     function($builder) {
-                        $builder->setTagName('page');
-                        $builder->setReplacementText('<a href="' . Url::to(['page/view', 'id' => '']) . '{option}" class="page-link">{param}</a>');
-                        $builder->setUseOption(true)->setParseContent(true);
-                        return $builder->build();
+                        return $this->pageTag($builder);
                     },
                     // HACK id указан пустым, как следствие он приклеится из значения {param}
                     [ 'page', '<a href="' . Url::to(['page/view', 'id' => '']) . '{param}" class="page-link">{param}</a>' ],
@@ -75,96 +72,5 @@ class Discussion extends \yii\db\ActiveRecord
             'content' => 'Content',
             'purified_content' => 'Purified Content',
         ];
-    }
-
-    public function getEncoded_content() {
-        return Html::encode($this->content);
-    }
-
-    public function getDom() {
-        if(!$this->_dom) {
-            if($this->purified_content === '' || $this->purified_content === null) {
-                if($this->content !== '' && $this->content !== null) {
-                    $this->save();
-                } else {
-                    return '';
-                }
-            }
-            return $this->_dom = new GlHtml($this->purified_content);
-        } else {
-            return $this->_dom;
-        }
-    }
-
-    public function purifiedContent()
-    {
-        $dom = $this->dom;
-        $html = $this->purified_content;
-        foreach (['linkHighlighting', 'headers'] as $key) {
-            $this->$key($dom, $html);
-        }
-        return $html;
-    }
-
-    public function linkHighlighting($dom, &$html)
-    {
-        $l = $dom->get('.page-link[href^="/web/index.php?r=page%2Fview&id="]');
-
-        if(count($l) == 0) return;
-        foreach($l as $i) {
-            parse_str(parse_url($i->getAttribute('href'), PHP_URL_QUERY), $output);
-            $links[] = $output['id'];
-            $c[] = [$i, $output['id']];
-        }
-        $exist = $this->find()->where(['in', 'title', $links])->all();
-        $e = [];
-        foreach($exist as $i) {
-           $e[] = $i->title;
-        }
-        foreach($c as $k) {
-            if(!in_array($k[1], $e)) {
-                $k[0]->setAttributes(['class' => 'page-doesnt-exist']);
-            }
-        }
-        $html = $dom->html();
-    }
-
-    public function array2Tree($arr, $n) {
-        $t = '';
-        foreach($arr as $i) {
-            if(is_array($i)) {
-                $t .= "<div class='indent'>{$this->array2Tree($i, $n + 1)}</div>";
-            } else {
-                $t .= "<div><a href='#$i'>$i</a></div>";
-            }
-        }
-        return $t;
-    }
-
-    private function arrayFromHeader(&$headers, $current, $value) {
-        if($current == 1) {
-            $headers[] = $value;
-        } else {
-            if(is_array(end($headers))) {
-                $this->arrayFromHeader($headers[count($headers) - 1], $current - 1, $value);
-            } else {
-                $headers[] = [$value];
-            }
-        }
-    }
-
-    public function headers($dom, &$html)
-    {
-        $l = $dom->get('h1, h2, h3, h4, h5, h6');
-        if(count($l) == 0) return;
-        $headers = [];
-        $last = 1;
-        $keys = ['h1' => 1, 'h2' => 2, 'h3' => 3, 'h4' => 4, 'h5' => 5, 'h6' => 6, ];
-        foreach($l as $i) {
-            $this->arrayFromHeader($headers, $keys[$i->getName()], $i->getText());
-            $i->replaceMe("<a name='{$i->getText()}'><{$i->getName()}>{$i->getText()}</{$i->getName()}></a>");
-        }
-        echo $this->array2Tree($headers, 0);
-        $html = $dom->html();
     }
 }
